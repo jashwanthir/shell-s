d51b1b973d117e98dffde568fe69aea691ac77f4
jashu <jashur@gmail.com>
2025-01-12 14:13:46 +0000

    spe-var

---
+++ specialvar.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+echo "all commands passed : $@"
+echo "number of variable : $#"
+echo "present working directory : $pwd"
+echo "home dirctory : $home"
+echo "script name : $0"
+echo "process id of current script : $$"
+echo "process id of last command in background : $!
+"
