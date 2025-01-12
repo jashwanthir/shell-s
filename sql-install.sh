e8b56da39d7338090b18001599a9680960fcf391
jashu <jashur@gmail.com>
2025-01-12 20:28:33 +0000

    install sql

--- sql-install.sh
+++ sql-install.sh
@@ -8,10 +8,10 @@ fi
 
 dnf list installed mysql
 
-if  [ $? -ne 0]
+if [ $? -ne 0 ]
 then
     dnf install mysql -y
-    if [ $? -ne 0]
+    if [ $? -ne 0 ]
     then 
         echo "mysql failed to install"
         exit 1
