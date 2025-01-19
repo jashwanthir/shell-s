5f3863147e00633a875e6091eedb8610ce55ea43
jashu <jashur@gmail.com>
2025-01-19 17:51:30 +0000

    logs

---
+++ logs.sh
@@ -0,0 +1,38 @@
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$( echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%D-%H-%S)
+LOG_FILE_NAME="LOG_FOLDER/LOG_FILE-TIME_STAMP.LOG" &>>$LOG_FILE_NAME
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo " $2 is failure "
+        exit1
+    else
+        echo " $2 is success " &>>$LOG_FILE_NAME
+    fi
+}
+echo "script started excuted at :: $TIMESTAMP " &>>$LOG_FILE_NAME
+
+if [ USERID -ne 0 ]
+then 
+    echo "error- need sudo access to excute "
+    exit1
+fi
+
+
+
+ if [ $? ne 0 ]
+ then 
+    dnf intall mysql -y &>>$LOG_FILE_NAME
+else
+    echo " mysql is already installed " &>>$LOG_FILE_NAME
+fi
+
+if [ $? ne 0 ]
+then 
+    dnf install git -y &>>$LOG_FILE_NAME
+else
+    echo " git is already installed " &>>$LOG_FILE_NAME
+fi 
