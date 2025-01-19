1013e149ac9a02f1f023d33c4257d20431285481
jashu <jashur@gmail.com>
2025-01-19 18:20:03 +0000

    log

--- logs.sh
+++ logs.sh
@@ -2,7 +2,7 @@ USERID=$(id -u)
 LOG_FOLDER="/var/log/shellscript-log"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%D-%H-%S)
-LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIME_STAMP.LOG" 
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIME_STAMP.log" 
 
 VALIDATE(){
     if [ $1 -ne 0 ]
