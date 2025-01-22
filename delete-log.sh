910bc6686bb9c6b4ddcd3b842d03d6b78ec18695
jashu <jashur@gmail.com>
2025-01-22 10:51:35 +0000

    DELETE-LOGS

--- delete-log.sh
+++ delete-log.sh
@@ -9,7 +9,7 @@ LOG_FILE=$( echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-SOURCE_DIR="/home/ec2-user/app-logs"
+SOURCE_DIR="/home/ec2-user/apps-log"
 
 VALIDATE()
 {
