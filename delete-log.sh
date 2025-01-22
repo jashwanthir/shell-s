043c915eb740a924780b92bdbc48756c7b462b36
jashu <jashur@gmail.com>
2025-01-22 10:41:02 +0000

    DELETE-LOGS

---
+++ delete-log.sh
@@ -0,0 +1,44 @@
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-log"
+LOG_FILE=$( echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+SOURCE_DIR="/home/ec2-user/app-logs"
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo " error:: need sudo acces to excute $R "
+        exit 1
+    fi
+} 
+echo "scrpit started excuting at : $TIMESTAMP " &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -mtime +14)
+echo "Files to be deleted: $FILES_TO_DELETE"
+
+while read -r X # here filepath is the variable name, you can give any name
+do
+    echo "Deleting file: $X" &>>$LOG_FILE_NAME
+    rm -rf $X
+    echo "Deleted file: $X"
+done <<< $FILES_TO_DELETE
